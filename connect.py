d3302c466fadab104a703d88facdaa1833b7060c
root <root@P24046-PC.iii.org.tw>
2024-02-29 11:04:17 +0800

    0229

--- connect.py
+++ connect.py
@@ -32,77 +32,67 @@ def create_table():
     conn.close()
     print("initial successfully")
     
-# 查询数据
+# 查询数据 (name為parameter)
 def select(name):
-    try:
-        cursor.execute(f"SELECT * FROM data WHERE name = '{name}' LIMIT 1;")
-        return cursor.fetchall()
-    except Exception as e:
-        return "Name is not exist"
-
+    cursor.execute(f"SELECT * FROM data WHERE name = '{name}';")
+    data = cursor.fetchall()
+    if name == '':
+        return 'Can not be empty'
+    elif data == []:
+        return 'Name is not exist'
+    else:
+        print(data)
+        return {'name': data[0][0], 'phone': data[0][1], 'email': data[0][2], 'address': data[0][3]}   
 # 插入数据
 def insert (name, phone, email, address):
-    try:
-        select(name)[0] # 可能会触发异常的函数调用
-        return "Name already exists"
-    except Exception  as e:
-        cursor.execute(f"INSERT INTO data VALUES ('{name}', '{phone}', '{email}', '{address}');")
-        return "Insert successfully"
+   _name = select(name)
+   if name == '' or phone == '' or email == '' or address == '':
+         return 'Any block cannot be empty'
+   elif _name == 'Name is not exist':
+         cursor.execute(f"INSERT INTO data VALUES ('{name}', '{phone}', '{email}', '{address}');")
+         return 'insert successfully'
+   else:
+        return 'Name is exist'
+   
     
 # 刪除数据(name為parameter)
 def delete(name):
-    cursor.execute(f"DELETE FROM data WHERE name = '{name}';")
-    return 'delete successfully'
+    _name = select(name)
+    if name == '':
+        return 'Can not be empty'
+    elif _name == 'Name is not exist':
+        return 'Name is not exist'
+    else:
+        cursor.execute(f"DELETE FROM data WHERE name = '{name}';")
+        return 'delete successfully'
   
-            
-            
-            
-            
-#streamlit 
+                
+#streamlit
 
+ 
+#插入資料
 st.title("Inter your information!")
-#新增輸入框
 name = st.text_input("Name",key="insert_name")
 phone = st.text_input("Phone",key="insert_phone")
 email = st.text_input("Email",key="insert_email")
 address = st.text_input("Address",key="insert_address")
-#按鈕
 button_insert = st.button("INSERT")
-
-#按鈕功能
-if button_insert:#插入資料
+if button_insert:
     st.write(insert(name, phone, email, address))
    
-   
-   
-   
+#刪除資料  
 st.title("Delete your information!")
 delete_name = st.text_input("Name",key="delete_name")
-
-#按鈕
 button_delete = st.button("DELETE")
-
-#按鈕功能
-if button_delete:#刪除資料
+if button_delete:
     st.write(delete(delete_name))
     
-   
-    
-    
-    
+#查詢資料  
 st.title("Find your information!")
 find_name = st.text_input("Name",key="find_name")
- 
-#按鈕    
 button_SELECT = st.button("SELECT") 
-#按鈕功能
-if button_SELECT:#查詢資料
+if button_SELECT:
     st.write(select(find_name))
     
-    
-    
-    
-
-
 if __name__ == "__main__":
     create_table()
